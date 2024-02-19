d14f227d7a7ef6b332fcb4f207e142f45fed02f2
Brainware PC <your_email@example.com>
2024-02-19 13:42:01 +0530

    College Works Posted }

---
+++ College Works Differently/1.py
@@ -0,0 +1,7 @@
+name=input("Enter Your Name??\n")
+marks=int(input("Enter Your Marks??\n"))
+if marks>40:
+    print(f"{name} Is Passed")
+else:
+    print(f"{name} Is Not Passed")
+
---
+++ College Works Differently/2.py
@@ -0,0 +1,21 @@
+Integer=10
+Float=10.5
+String="Ayan"
+List=["Hello World",90]
+Tuple=("Hello World",90)
+Dictionary={
+    "Key_1":"Value_1",
+    "Key_2":"Value_2",
+}
+Boolean=True
+Complex=6+5j
+Set=set(List)
+print(type(Integer))
+print(type(Dictionary))
+print(type(Boolean))
+print(type(Complex))
+print(type(Set))
+print(type(Float))
+print(type(String))
+print(type(List))
+print(type(Tuple))
