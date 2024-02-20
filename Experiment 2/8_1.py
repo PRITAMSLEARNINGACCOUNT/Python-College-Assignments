58e0a556c93c9e3bd2251851c83251ecbbb75ff2
pritamstech <pritamstech@hotmail.com>
2024-02-20 10:35:49 +0530

    Experiment 8 Completely Done

--- Experiment 2/8_1.py
+++ Experiment 2/8_1.py
@@ -3,5 +3,6 @@ Principal = float(input("Enter The Principal Amount??\n"))
 Rate = float(input("Enter The Interest Rate??\n"))
 Interest = 0.0
 for i in range(Time):
-    Principal += ((Principal*Rate)/100)
+    Interest = ((Principal*Rate)/100)
+    Principal += Interest
 print(f"So The Compound Interest Amount Is {Interest+Principal}")
